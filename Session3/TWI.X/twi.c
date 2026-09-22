49c5178971ab9c3ec9f4358296f94d0fc1cab739
Emma Langeland <elangeland@hotmail.no>
2026-09-22 20:05:37 +0200

    work on TWI task

--- Session3/TWI.X/twi.c
+++ Session3/TWI.X/twi.c
@@ -2,23 +2,42 @@
 #include <avr/io.h>
 #include <stdint.h>
 
+#define SDA_PIN PIN2_bm //2
+#define SCL_PIN PIN2_bm //3
+
+// Define the TWI frequency
+#define TWI_FREQ 100000  // Standard TWI frequency
+#define F_CPU 4000000UL
+
+#define TWI_BAUD F_CPU/(2*TWI_FREQ) -5
+
 void TWI_init() {
     // Initialize the pins as outputs
+
+    PORTA.DIRSET = SDA_PIN;
+    PORTA.DIRSET = SCL_PIN;
+    /*equivalent to:
+    PORTA.DIR |= (1 << SDA) ;
+    PORTA.DIR |= (1 << SCL) ; */
 
     // Set the TWI clock frequency
     // The formula to calculate the clock is found
     // in the AVR128DB48 datasheet on page 425.
     // Feel free to use the F_CPU and TWI_FREQ values
     // defined in the header file.
+    TWI0.MBAUD = TWI_BAUD ;
 
     // Set the bus state as idle
     // It defauts to being in an unknown state
     // when the device is first powered on.
     // This is why we need to set it to idle
     // See the TWI0.MSTATUS register.
+    TWI0.MSTATUS = TWI_BUSSTATE_IDLE_gc;
 
     // Enable TWI in the master mode
     // See the TWI0.MCTRLA register.
+    TWI0.MCTRLA = TWI_ENABLE_bm; //| TWI_WIEN_bm;
+
 }
 
 void TWI_load_addr(uint8_t device_address, bool write) {
@@ -28,6 +47,10 @@ void TWI_load_addr(uint8_t device_address, bool write) {
     //
     // NBNB: We need to wait for the write interrupt flag when we are
     //       loading the device address for writing.
+    if(TWI0.MSTATUS & TWI_WIF_bm){ 
+        TWI0.MADDR = (device_address << 1) ;
+        TWI0.MADDR |= write ;
+    }
 }
 
 void TWI_write_data(uint8_t *data, uint16_t len) {
